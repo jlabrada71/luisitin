7ba8375634a03f8670e59b9962495527d52fbe0a
Juan Ramón Labrada Estrada <70043742+jlabrada71@users.noreply.github.com>
2023-03-21 10:43:16 -0300

    Update copy_ssh.sh

--- copy_ssh.sh
+++ copy_ssh.sh
@@ -1,3 +1,3 @@
 echo "usage: copy_ssh.sh <directory> <file> <key.pem> <server> <user>"
-echo "ex: copy_ssh.sh app file ~/.ssh/app.pem 3.15.3.12 ubuntu
+echo "ex: copy_ssh.sh app file ~/.ssh/app.pem 3.15.3.12 ubuntu"
 scp -i $3 $1/$2 $5@$4:/home/$5/pending-deploy/$2
