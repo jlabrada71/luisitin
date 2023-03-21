1ceb8b1043ba20b535a6d420607de4fdb46f1713
Juan Ramón Labrada Estrada <70043742+jlabrada71@users.noreply.github.com>
2023-03-21 10:08:16 -0300

    Update copy_ssh_setup.sh

--- copy_ssh_setup.sh
+++ copy_ssh_setup.sh
@@ -1,1 +1,4 @@
-scp -i ~/.ssh/aws-juanlabrada.com.pem server-domain-setup/* ubuntu@3.15.9.164:/home/ubuntu/setup-files
+echo "usage: copy_ssh_setup.sh <privatekeyfile> <targetserver> <user>"
+echo "ex: copy_ssh_setup.sh ~/.ssh/app.pem 2.4.2.4 ubuntu"
+
+scp -i $1 server-domain-setup/* $3@$2:/home/$3/setup-files
