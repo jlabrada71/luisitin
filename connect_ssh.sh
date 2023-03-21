bb1e45d6d9509895e5fe1bcc02eb10230a5e1087
Juan Ramón Labrada Estrada <70043742+jlabrada71@users.noreply.github.com>
2023-03-21 10:45:59 -0300

    Update connect_ssh.sh

--- connect_ssh.sh
+++ connect_ssh.sh
@@ -1,1 +1,3 @@
-ssh -i ~/.ssh/aws-juanlabrada.com.pem ubuntu@ec2-3-15-9-164.us-east-2.compute.amazonaws.com
+echo "usage: connect_ssh.sh <key.pem> <user> <server>"
+echo "ex: connect_ssh.sh ~/.ssh/app.pem ubuntu  ec2-3-15-9.us-east.amazonaws.com"
+ssh -i $1 $2@$3
